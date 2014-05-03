0435fdca50476c77c0f3592d016320c5ceef1c6a
idowolf <idowolf@gmail.com>
2014-05-03 17:11:08 +0000

    Disclaimer n stuff

--- MilbatProject/DisclaimerPage.xaml.cs
+++ MilbatProject/DisclaimerPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using MilbatProject.ViewModels;
 
 namespace MilbatProject
 {
@@ -15,6 +16,33 @@ namespace MilbatProject
         public DisclaimerPage()
         {
             InitializeComponent();
+        }
+
+        private void UserDisclaimerSign_Checked(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if(DisclaimerPageViewModel.IsDisclaimerSigned())
+            {
+                UserDisclaimerSign.IsChecked = true;
+                UserDisclaimerSign.IsEnabled = false;
+                UserDisclaimerSign.Content = "הנך אישרת את התקנון.";
+            }
+        }
+
+        private void ApplicationBarIconButton_Click(object sender, EventArgs e)
+        {
+            if((bool)UserDisclaimerSign.IsChecked)
+            {
+                if (!DisclaimerPageViewModel.IsDisclaimerSigned())
+                {
+                    UserDisclaimerSign.IsChecked = true;
+                    DisclaimerPageViewModel.SignDisclaimer();
+                    UserDisclaimerSign.Content = "תודה על אישור התקנון.";
+                }
+            }
         }
     }
 }
